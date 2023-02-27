8eb3a907d64ccd561ffc5ba0cd904b7ee9c1eebc
d3monv3num <namananand891@gmail.com>
2023-02-27 14:35:02 +0530

    dynamic content render on dashboard profile page

--- controllers/routes/routing.js
+++ controllers/routes/routing.js
@@ -4,6 +4,7 @@ const bodyparse=require('body-parser')
 const getDB=require('../../utils/database').getDB;
 const hashfunction=require('../../models/hashfunctionmodel').hashfunction;
 const router=express.Router()
+let student_details;
 
 // bodyparser to take input from forms
 router.use(bodyparse.urlencoded({extended:true}));
@@ -19,7 +20,22 @@ router.get('/registration.html',(req,res,next)=>{
     res.sendFile(path.join(__dirname,"..","..","views","registration.html"))
 })
 router.get('/personal_details',(req,res,next)=>{
-    res.render('personal_details')
+    res.render('personal_details',{
+            st_fname:student_details.fname,
+            st_name:student_details.fname+' '+student_details.lname,
+            st_id:student_details.id,
+            st_dept:student_details.department,
+            st_contact:student_details.mobile,
+            st_mail:student_details.email,
+            st_doj:student_details.doj,
+            st_gender:student_details.gender,
+            st_dob:student_details.dob,
+            st_aadhar:student_details.aadhar,
+            st_paddress:student_details.paddress1,
+            st_fathername:student_details.fathername,
+            st_mothername:student_details.mothername,
+            st_state:student_details.state
+    })
 })
 router.get('/addform',(req,res,next)=>{
     res.sendFile(path.join(__dirname,"..","..","views","addform.html"));
@@ -34,11 +50,19 @@ router.post('/index.html',(req,res,next)=>{
     // searching for the user
     const currstudent=db.collection('studentrecord').find({id:userid,loginpassword:password}).next()
     .then(studentrecord=>{
+            student_details=studentrecord;
             console.log(studentrecord);
             if(studentrecord==null){
                 // user not found
                 res.sendFile(path.join(__dirname,'..','..','views','index.html'));
-            }else res.render('dashboard');//user found
+            }else res.render('dashboard',{
+                st_fname:studentrecord.fname,
+                st_name:studentrecord.fname+' '+studentrecord.lname,
+                st_id:studentrecord.id,
+                st_dept:studentrecord.department,
+                st_contact:studentrecord.mobile,
+                st_mail:studentrecord.email
+            });//user found
             return studentrecord;  
     }).catch(err=>{
         console.log(err);
